dcc3bb5b310350b170835b853bd656ede19fb6b2
YingzhePeng <yingzhepeng@foxmail.com>
2023-04-24 01:50:32 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ModaFew',
+    version='0.1',
+    description='The package for laoding Large Model to do Few-shot/Zero-shot Inference',
+    author='YingzhePeng',
+    author_email='yingzhepeng@foxmail.com',
+    packages=find_packages()
+)
