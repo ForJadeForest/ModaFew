dc6eafb77204283d87857b5699bbda57f39326bb
ForJadeForest <yingzhepeng@foxmail.com>
2023-05-22 21:32:15 +0800

    use MiniGPT4 class to init

--- ModaFew/minigpt4_interface/load_minigpt4.py
+++ ModaFew/minigpt4_interface/load_minigpt4.py
@@ -1,29 +1,59 @@
+import os
 import torch
-import numpy as np
+import pathlib
 
 from typing import Union, List
 from PIL.Image import Image
+from omegaconf import OmegaConf
 
 from minigpt4.common.config import Config
 from minigpt4.common.registry import registry
 from minigpt4.conversation.conversation import Chat, CONV_VISION
+from minigpt4.models import MiniGPT4
+from minigpt4.processors import Blip2ImageEvalProcessor
+
+
+def read_default_config(config_path, prompt_path, minigpt4_path, vicuna_path):
+    if config_path is not None:
+        config = OmegaConf.load(config_path)
+        if minigpt4_path:
+            config.model['ckpt'] = minigpt4_path
+        if vicuna_path:
+            config.model['llama_model'] = vicuna_path
+        return config
+
+    file_path = pathlib.Path(__file__).resolve()
+    root_dir = file_path.parents[2]
+    minigpt4_repo_path = root_dir / 'requirements_repo' / 'MiniGPT-4'
+    config_path = minigpt4_repo_path / 'eval_configs' / 'minigpt4_eval.yaml'
+
+    config = OmegaConf.load(str(config_path))
+    
+    config.model['ckpt'] = minigpt4_path
+    config.model['llama_model'] = vicuna_path
+    return config
 
 
 class MiniGPT4Interface:
-    def __init__(self, config_path, gpu_id, **kwargs):
-        print('Initializing Chat')
-        gpu_id = int(gpu_id)
-        self.cfg = Config(config_path, **kwargs)
+    def __init__(self, device, 
+                 config_path=None, 
+                 prompt_path=None,
+                 minigpt4_path=None,
+                 vicuna_path=None,
+                 **kwargs):
+        config = read_default_config(config_path, prompt_path, minigpt4_path, vicuna_path)
+        user_config = OmegaConf.create(kwargs)
+        config.options = user_config
+        
+        self.cfg = Config(config)
         model_config = self.cfg.model_cfg
-        model_config.device_8bit = gpu_id
-        # TODO: 不使用 model_cls.from_config，直接从类初始化
-        model_cls = registry.get_model_class(model_config.arch)
-        model = model_cls.from_config(model_config).to('cuda:{}'.format(gpu_id))
-
-
+        self.model = MiniGPT4.from_config(model_config).to(device)
+        self.model.eval()
+        
         vis_processor_cfg = self.cfg.datasets_cfg.cc_sbu_align.vis_processor.train
-        vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
-        self.chat = Chat(model, vis_processor, device='cuda:{}'.format(gpu_id))
+        vis_processor = Blip2ImageEvalProcessor.from_config(vis_processor_cfg)
+        self.chat = Chat(self.model, vis_processor, device=device)
+        
         self.conversation_history = CONV_VISION.copy()
         self.image_list = []
         print('Initialization Finished')
@@ -40,6 +70,7 @@ class MiniGPT4Interface:
         answer = self.chat.answer(self.conversation_history, self.image_list, **kwargs)[0]
         return answer
         
+    @torch.no_grad()
     def zero_shot_generation(self, 
                              image: Union[Image, str, torch.Tensor], 
                              query: str ='', 
@@ -49,6 +80,7 @@ class MiniGPT4Interface:
         self.reset()
         return answer
 
+    @torch.no_grad()
     def few_shot_generation(self, 
                             example_images: List[Union[Image, str, torch.Tensor]], 
                             example_texts: List[str], 
