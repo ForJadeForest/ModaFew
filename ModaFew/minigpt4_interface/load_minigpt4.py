01f1313efe9f04c79415ba6ddd7ccbbb0a6e425a
ForJadeForest <yingzhepeng@foxmail.com>
2023-05-23 10:17:03 +0800

    add new interface

--- ModaFew/minigpt4_interface/load_minigpt4.py
+++ ModaFew/minigpt4_interface/load_minigpt4.py
@@ -2,6 +2,7 @@ import os
 import torch
 import pathlib
 
+from ModaFew.utils import image2tensor
 from typing import Union, List
 from PIL.Image import Image
 from omegaconf import OmegaConf
@@ -34,7 +35,7 @@ def read_default_config(config_path, prompt_path, minigpt4_path, vicuna_path):
     return config
 
 
-class MiniGPT4Interface:
+class MiniGPT4ChatInterface:
     def __init__(self, device, 
                  config_path=None, 
                  prompt_path=None,
@@ -102,9 +103,115 @@ class MiniGPT4Interface:
         for input_image in input_images:
             self.chat.upload_img(input_image, self.conversation_history, self.image_list)
             self.chat.ask(query, self.conversation_history)
-            output_text = self.chat.answer(self.conversation_history, self.image_list, **kwargs)[0]
+            output_text = self.chat.answxer(self.conversation_history, self.image_list, **kwargs)[0]
         self.reset()
         return output_text
+    
+    def vqa_prompt(self, question, answer=None) -> str:
+        return f"Question:{question} Short answer:{answer if answer is not None else ''}"
+
+
+
+class MiniGPT4Interface:
+    def __init__(self, device, 
+                 config_path=None, 
+                 prompt_path=None,
+                 minigpt4_path=None,
+                 vicuna_path=None,
+                 **kwargs):
+        config = read_default_config(config_path, prompt_path, minigpt4_path, vicuna_path)
+        user_config = OmegaConf.create(kwargs)
+        config.options = user_config
+        
+        self.device = device
+        self.cfg = Config(config)
+        model_config = self.cfg.model_cfg
+        self.model = MiniGPT4.from_config(model_config).to(device)
+        self.model.eval()
+        
+        vis_processor_cfg = self.cfg.datasets_cfg.cc_sbu_align.vis_processor.train
+        self.vis_processor = Blip2ImageEvalProcessor.from_config(vis_processor_cfg)
+        # self.chat = Chat(self.model, svis_processor, device=device)
+        
+        self.conversation_history = CONV_VISION.copy()
+        self.image_list = []
+        self.system_prompt = "Give the following image: <Img>ImageContent</Img>. "\
+                            "You will be able to see the image once I provide it to you. Please answer my questions.###"
+        print('Initialization Finished')
+
+    
+    def reset(self):
+        self.conversation_history = CONV_VISION.copy()
+        self.image_list = []
+
+    def _chat_one_time(self, image, query, **kwargs):
+        if image:
+            self.chat.upload_img(image, self.conversation_history, self.image_list)
+        self.chat.ask(query, self.conversation_history)
+        answer = self.chat.answer(self.conversation_history, self.image_list, **kwargs)[0]
+        return answer
+
+
+
+    def process_context(self, context_images, context_texts, input_images, query):
+        """
+        对batch中一个元素生成图像的embedding
+        """
+        context_images = [image2tensor(img, self.vis_processor).to(self.device)  for img in context_images]
+        context_images = torch.stack(context_images, dim=0)
+        image_embeds, _ = self.model.encode_img(context_images)
+        assert len(image_embeds.shape) == 2
+        
+        text_prompt = self.system_prompt
+        text_prompt = ''.join(text_prompt)
+        
+        prompt_segs = text_prompt.split('<ImageHere>')
+        assert len(prompt_segs) == len(image_embeds) + 1, "Unmatched numbers of image placeholders and images."
+        seg_tokens = [
+            self.model.llama_tokenizer(
+                seg, return_tensors="pt", add_special_tokens=i == 0).to(self.device).input_ids
+            # only add bos to the first seg
+            for i, seg in enumerate(prompt_segs)
+        ]
+        seg_embs = [self.model.llama_model.model.embed_tokens(seg_t) for seg_t in seg_tokens]
+        mixed_embs = [emb for pair in zip(seg_embs[:-1], image_embeds) for emb in pair] + [seg_embs[-1]]
+        mixed_embs = torch.cat(mixed_embs, dim=1)
+        return mixed_embs
+        
+        
+        
+        
+
+    @torch.no_grad()
+    def few_shot_generation(self, 
+                            example_images: List[Union[Image, str, torch.Tensor]], 
+                            example_texts: List[str], 
+                            input_images: Union[List[Union[Image, str, torch.Tensor]], Image, str, torch.Tensor], 
+                            query: str ='', 
+                            **kwargs):
+        assert len(example_images) == len(example_texts), f"The few-shot image should num should be the same as the num of example_texts"
+        few_shot_num = len(example_texts)
+        for i in range(few_shot_num):
+            self.chat.upload_img(example_images[i], self.conversation_history, self.image_list)
+            self.chat.ask(query, self.conversation_history)
+            self.conversation_history.append_message(self.conversation_history.roles[1], example_texts[i])
+        
+        if not isinstance(input_images, List):
+            input_images = [input_images]
+
+        assert len(input_images) == 1, f"Now only support one image as input"
+
+        for input_image in input_images:
+            self.chat.upload_img(input_image, self.conversation_history, self.image_list)
+            self.chat.ask(query, self.conversation_history)
+            output_text = self.chat.answxer(self.conversation_history, self.image_list, **kwargs)[0]
+        self.reset()
+        return output_text
+    
+    def vqa_prompt(self, question, answer=None) -> str:
+        return f"Question:{question} Short answer:{answer if answer is not None else ''}"
+
+
     
 """
 Conversation.messages:
@@ -120,7 +227,8 @@ Conversation.messages:
 Prompt = Give the following image: <Img>ImageContent</Img>. You will be able to see the image once I provide it to you. Please answer my questions.###Human: <Img><ImageHere></Img> Describe the image###Assistant:
 
 get_context_emb
-Prompt = Give the following image: <Img>ImageContent</Img>. You will be able to see the image once I provide it to you. Please answer my questions.###Human: <Img><ImageHere></Img> Describe the image###Assistant:
+Prompt = Give the following image: <Img>ImageContent</Img>. You will be able to see the image once I provide it to you. Please answer my questions.###
+Human: <Img><ImageHere></Img> Describe the image###Assistant:
 prompt_segs = ["Give the following image: <Img>ImageContent</Img>. You will be able to see the image once I provide it to you. Please answer my questions.###Human: <Img>", "</Img> Describe the image###Assistant:"]
 
 after get the answer 
