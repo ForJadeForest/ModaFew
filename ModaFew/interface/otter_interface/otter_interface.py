dba21e9f9e169b72f357132bac6decc18f3e188d
ForJadeForest <yingzhepeng@foxmail.com>
2023-06-16 13:58:51 +0800

    init otter

---
+++ ModaFew/interface/otter_interface/otter_interface.py
@@ -0,0 +1,6 @@
+from otter import OtterModel
+from ModaFew.interface.base_interface import BaseInterface
+from ModaFew.interface.utils import IMAGE_TYPE, image2tensor
+
+class OtterInterface(BaseInterface):
+    def __init__()
